3ce0cca46d692d7b0a85f7f39a768c65e134ca91
Smileatmaxi <87715331+Smileatmaxi@users.noreply.github.com>
2025-02-23 03:11:16 +0100

    Add initial homepage content structure
    Introduce a basic layout for the homepage, including a title, description, and placeholders for information about game availability and engine details. This provides a foundation for further design and content improvements.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+"use client"
+
 import React from "react";
 
 export default function Home() {
     return (
-        <div>
-        </div>
+        <section>
+            <div className="">
+                <div className="">
+                    <div></div>
+                    <div className="mt-5">
+                    </div>
+                    <h1 className="opacity-85 text-6xl text-center pb-20 font-bold">Survive the Ancient Horrors</h1>
+                    <p className="opacity-85 text-center text-2xl pb-20">Ancient Survival Horror Adventure</p>
+                    <div className="flex justify-center gap-10">
+                        <p className="opacity-85 text-center pb-20">Coming soon to Steam</p>
+                        <p className="opacity-85 text-center pb-20">Coming soon to Epic Games</p>
+                    </div>
+                    <p className="opacity-85 text-center">Powered by Unreal Engine</p>
+                </div>
+            </div>
+        </section>
     );
 }
