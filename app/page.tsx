8a0b240518cd45bb011af8ea444147daf52f53fe
Smileatmaxi <Maxi.Leupold@outlook.de>
2025-03-01 01:35:35 +0100

    added second section to main Page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 export default function Home() {
     return (
+        <div>
         <section className="w-full block relative overflow-hidden bg-no-repeat bg-fixed bg-center bg-cover">
             <div className="min-h-screen flex items-center pt-6.5 pb-6.5">
                 <div className="w-full relative text-center">
@@ -20,5 +21,25 @@ export default function Home() {
                 </div>
             </div>
         </section>
+            <section>
+                <div className="min-h-screen flex items-center pt-6.5 pb-6.5">
+                    <div className="w-full relative text-center">
+                        <div></div>
+                        <div className="mt-5">
+                        </div>
+                        <h1 className="opacity-85 text-7xl text-center pb-20 font-bold">Escape the Maze of Horrors</h1>
+                        <p className="opacity-85 text-center text-2xl pb-20 ml-10 mr-10">
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                            et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                            diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+                            voluptua. At
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
+
     );
 }
