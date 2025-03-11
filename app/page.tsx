05728ed968aba5c4b151d39565f64be6cded12a5
Maximilian Leupold <Maximilian.Leupold@outlook.de>
2025-03-11 23:15:57 +0100

    removed used client removed Image import for now and added placeholder pics to see results for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,60 @@
-"use client"
-
 import React from "react";
-import Image from "next/image";
-import vercel from "../public/vercel.svg"
 
 export default function Home() {
     return (
         <div>
-            <div>
-                <Image src={vercel} alt="test" layout="fill" objectFit="cover"/>
-            <section className="w-full block relative overflow-hidden bg-no-repeat bg-fixed bg-center bg-cover">
+            <section className="bg-sectionOne relative block overflow-hidden bg-no-repeat bg-cover bg-center bg-fixed">
                 <div className="min-h-screen flex items-center pt-6.5 pb-6.5">
                     <div className="w-full relative text-center">
-                        <div></div>
                         <div className="mt-5">
                         </div>
-                        <h1 className="opacity-85 text-7xl text-center pb-20 font-bold">Survive the Ancient Aztec
-                            Horrors</h1>
-                        <p className="opacity-85 text-center text-2xl pb-20">Ancient Survival Horror Adventure</p>
+                        <span className="text-7xl text-center pb-20 font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Survive the Ancient AztecHorrors
+                        </span>
+                        <p className="opacity-85 text-center text-2xl pb-20 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Ancient Survival Horror Adventure</p>
                         <div className="flex justify-center gap-10">
-                            <p className="opacity-85 text-center pb-20">Coming soon to Steam</p>
-                            <p className="opacity-85 text-center pb-20">Coming soon to Epic Games</p>
+                            <p className="opacity-85 text-center pb-20 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Coming soon to Steam</p>
+                            <p className="opacity-85 text-center pb-20 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Coming soon to Epic Games</p>
                         </div>
-                        <p className="opacity-85 text-center">Powered by Unreal Engine</p>
+                        <p className="opacity-85 text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Powered by Unreal Engine</p>
                     </div>
                 </div>
             </section>
-            </div>
-            <section>
+            <hr className="border-gray-800"></hr>
+            <section className="bg-sectionTwo relative block overflow-hidden bg-no-repeat bg-cover bg-center bg-fixed">
                 <div className="min-h-screen flex items-center pt-6.5 pb-6.5">
                     <div className="w-full relative text-center">
                         <div></div>
                         <div className="mt-5">
                         </div>
-                        <h1 className="opacity-85 text-7xl text-center pb-20 font-bold">Escape the Maze of Horrors</h1>
-                        <p className="opacity-85 text-center text-2xl pb-20 ml-10 mr-10">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                            et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                        <h1 className="opacity-85 text-7xl text-center pb-20 font-bold pl-4 pr-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Escape the Maze of Horrors</h1>
+                        <p className="opacity-85 text-center text-2xl pb-20 ml-10 mr-10 pl-6 pr-6 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                            tempor
+                            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+                            accusam
+                            et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                            est
+                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                            sed
                             diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
                             voluptua. At
                         </p>
                     </div>
                 </div>
             </section>
-            <section>
+            <hr className="border-gray-800"></hr>
+            <section className="bg-sectionThree relative block overflow-hidden bg-no-repeat bg-cover bg-center bg-fixed">
                 <div className="min-h-screen flex items-center pt-6.5 pb-6.5">
                     <div className="w-full relative text-center">
                         <div></div>
                         <div className="mt-5">
                         </div>
-                        <h1 className="opacity-85 text-7xl text-center pb-20 font-bold">Potential Screenshots</h1>
+                        <h1 className="opacity-85 text-7xl text-center pb-20 font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Potential Screenshots</h1>
                         <div className="relative">
                         </div>
                     </div>
                 </div>
             </section>
         </div>
-
     );
 }
