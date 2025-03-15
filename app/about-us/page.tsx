c775372eb5761f0642573e74b24d62734f9ca664
Maximilian Leupold <Maximilian.Leupold@outlook.de>
2025-03-15 22:47:35 +0100

    feat: added some text for now

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -4,9 +4,15 @@ import {AnimatedTestimonialsTeam} from "@/components/ui/team-testimonials";
 const AboutUs = () => {
     return (
         <div className="h-screen mx-auto content-center">
-            <div className="text-center">
+            <div className="text-center mb-36">
                 <h1 className="text-7xl font-semibold tracking-tight text-balance text-white sm:text-5xl">Who are
                     we?</h1>
+                <span className="">
+                    <p>
+                        We Are Illuminated Interactive a Small Game studio from Germany we enjoy creating Games most of them will be in the Genre of Horror
+                        we really try our best to get as much Detail into our
+                    </p>
+                </span>
             </div>
             <div className="text-center">
                 <h1 className="text-7xl font-semibold tracking-tight text-balance text-white sm:text-5xl">Get to Know
