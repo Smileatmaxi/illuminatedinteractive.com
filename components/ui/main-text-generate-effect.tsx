415f594d325d3d3b116b08fb8bc1ac4a383e29c0
Maximilian Leupold <Maximilian.Leupold@outlook.de>
2025-03-16 16:43:25 +0100

    feat added Text Generation Effect for landingpage

---
+++ components/ui/main-text-generate-effect.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { TextGenerateEffect } from "@/components/ui/text-generate.effect";
+
+const words = `Survive the Ancient AztecHorrors`;
+
+
+export function TextGenerateEffectDemo() {
+    return <TextGenerateEffect words={words} />;
+}
