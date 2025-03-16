979bd3e6c89eef91f00aed9876816791eaccfa2e
Maximilian Leupold <Maximilian.Leupold@outlook.de>
2025-03-16 16:43:55 +0100

    feat: added new comps to faq page

--- app/frequently-asked/page.tsx
+++ app/frequently-asked/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const FrequentlyAsked = () => {
     return (
-        <div>FrequentlyAsked</div>
+        <div className="h-screen mx-auto content-center">
+            <div className="container"></div>
+        </div>
     )
 }
 export default FrequentlyAsked
