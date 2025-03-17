32bf724be52e93847ca1c2eaac80b2598d8ea435
Maximilian Leupold <Maximilian.Leupold@outlook.de>
2025-03-17 16:14:40 +0100

    fix: fixed compiling errors while building

--- components/ui/text-generate.effect.tsx
+++ components/ui/text-generate.effect.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import { useEffect } from "react";
+import {useEffect} from "react";
 import { motion, stagger, useAnimate } from "motion/react";
-import { cn } from "@/lib/utils";
+import {cn} from "@/lib/utils";
 
-export const TextGenerateEffect = ({
-                                       words,
-                                       className,
-                                       filter = true,
-                                       duration = 1,
-                                   }: {
+export const TextGenerateEffect = ({words, className, filter = true, duration = 1,}: {
     words: string;
     className?: string;
     filter?: boolean;
     duration?: number;
 }) => {
+
     const [scope, animate] = useAnimate();
     const wordsArray = words.split(" ");
+
     useEffect(() => {
         animate(
             "span",
@@ -25,11 +22,11 @@ export const TextGenerateEffect = ({
                 filter: filter ? "blur(0px)" : "none",
             },
             {
-                duration: duration ? duration : 1,
+                duration: duration,
                 delay: stagger(0.2),
             }
         );
-    }, [scope.current]);
+    }, [animate, duration, filter]);
 
     const renderWords = () => {
         return (
