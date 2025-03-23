66aa26776f549976e83328f121ac814dda3559ea
Maximilian Leupold <Maximilian.Leupold@outlook.de>
2025-03-23 20:06:18 +0100

    feat: changed to script

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import React from "react";
 import {Toaster} from 'sonner';
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-import Script from "next/script";
 
 const exo = Exo({
     subsets: ["latin"]
@@ -21,7 +20,7 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
     return (
         <html lang="en">
         <head>
-            <Script id="usercentrics-cmp" src="https://web.cmp.usercentrics.eu/ui/loader.js" data-draft="true" data-settings-id="XUJm6tLW5us2K1" async></Script>
+            <script id="usercentrics-cmp" src="https://web.cmp.usercentrics.eu/ui/loader.js" data-draft="true" data-settings-id="XUJm6tLW5us2K1" async></script>
         </head>
         <body className={`${exo.className} antialiased flex flex-col min-h-screen`}>
         <div className="">
